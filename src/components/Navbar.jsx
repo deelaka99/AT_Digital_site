716f6a45707696dd040095873f67d3d40e8640ed
deelaka99 <123wejith@gmail.com>
2023-12-22 23:59:59 +0530

    added logo for footer for mobiles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import Logo from '../assets/pictures/logo.png';
+import React from "react";
+import Logo from "../assets/pictures/logo.png";
 
 function Navbar() {
   return (
-    <div className='w-full mobile:h-[10vh] bg-primary'>
-      Navabar
+    <div className="w-full mobile:h-[10vh] bg-primary flex">
+      <div className="h-full mobile:w-3/4 flex items-center justify-start mobile:pl-3 mobile:pt-2 mobile:pb-2">
+        <img className="mobile:w-5/6 mobile:h-5/6" src={Logo} alt="logo" />
+      </div>
+      <div className="h-full mobile:w-1/4 flex items-center justify-end bg-success"></div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
