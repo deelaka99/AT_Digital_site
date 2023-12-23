88b40bfd9a45fe74ff4c36639f7fe722d77ab058
deelaka99 <123wejith@gmail.com>
2023-12-23 10:13:04 +0530

    added tablet responsivenes for navbar and home

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="h-screen w-full bg-secondary">
+    <div className="h-screen w-full">
       <Navbar />
       <Home />
       <Services />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,11 +3,11 @@ import Girl from "../assets/pictures/girl.jpg";
 
 function Home() {
   return (
-    <div className="w-full mobile:h-[100vh]">
+    <div className="w-full mobile:h-[100vh] tablet:h-[100vh]">
       {/* Home fraction 1 for navbar gap filling */}
-      <div className="w-full mobile:h-[10vh] flex items-center justify-center"></div>
+      <div className="w-full mobile:h-[10vh] tablet:h-[8vh] flex items-center justify-center"></div>
       {/* Home fraction 2 for girl image */}
-      <div className="w-full mobile:h-[40vh] flex items-center justify-center">
+      <div className="w-full mobile:h-[40vh] tablet:h-[62vh] flex items-center justify-center">
         {/* In here, i flipped the image using 'transform scale-x-[-1]' classes*/}
         <img
           className="h-full w-full transform scale-x-[-1]"
@@ -16,20 +16,12 @@ function Home() {
         />
       </div>
       {/* Home fraction 3 for intro paragraph */}
-      <div className="bg-gradient-to-r from-dark to-analogous2  w-full mobile:h-[50vh] mobile:px-6 mobile:py-7">
-        <p className="font-inter font-bold text-accent1 mobile:text-[36px] mobile:leading-[1]">
-          We Crush Your
-          <br />
-          Competitors,
-          <br />
-          Goals, And Sales
-          <br />
-          Records -Without
-          <br />
-          The B.S
+      <div className="bg-gradient-to-r from-dark to-analogous2 w-full mobile:h-[50vh] tablet:h-[30vh] mobile:px-6 mobile:py-7 tablet:px-7 tablet:py-6">
+        <p className="font-inter font-bold text-accent1 mobile:text-[36px] mobile:leading-[1.1] tablet:text-[43px] tablet:leading-[1.2]">
+          We Crush Your Competitors, Goals, And Sales Records -Without The B.S
         </p>
         <p>&nbsp;</p>
-        <button className="bg-secondary font-inter font-semibold mobile:text-[14px] text-accent1 mobile:py-3 mobile:px-6 rounded shadow-md active:bg-txtColorLight">
+        <button className="bg-secondary font-inter font-semibold mobile:text-[14px] tablet:text-[16px] text-accent1 mobile:py-3 mobile:px-6 tablet:py-4 tablet:px-7 rounded shadow-md active:bg-txtColorLight">
           GET FREE CONSULTATION
         </button>
       </div>
