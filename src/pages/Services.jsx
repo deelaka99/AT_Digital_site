d1ea0a86673c107fff0cc03feab4dc4d6ed182bb
deelaka99 <123wejith@gmail.com>
2023-12-23 13:40:45 +0530

    added laptop responsiveness for services component

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -4,20 +4,20 @@ import Magnifier from "../assets/pictures/magnifier.png";
 
 function Services() {
   return (
-    <div className="w-full mobile:h-[180vh] tablet:h-[80vh]">
+    <div className="w-full mobile:h-[180vh] tablet:h-[80vh] laptop:h-[150vh] laptop:px-28">
       {/* Service 1 fraction:- web & mobile app development part */}
-      <div className="bg-white w-full mobile:h-[90vh] tablet:h-[40vh] tablet:flex tablet:px-7">
+      <div className="bg-white w-full mobile:h-[90vh] tablet:h-[40vh] laptop:h-[75vh] tablet:flex tablet:px-7 laptop:px-0">
         {/* for PC image */}
-        <div className="mobile:w-full mobile:h-[30vh] tablet:h-full tablet:w-2/5 mobile:px-2 mobile:py-2 flex items-center justify-center">
+        <div className="mobile:w-full mobile:h-[30vh] tablet:h-full tablet:w-2/5 laptop:w-1/3 mobile:px-2 mobile:py-2 flex items-center justify-center">
           <img src={PC} alt="PC" className="mobile:w-2/3 tablet:w-full" />
         </div>
         {/* for web & mobile app development part */}
-        <div className="mobile:w-full mobile:h-[60vh] tablet:h-full tablet:w-3/5 flex flex-col mobile:items-center tablet:items-start justify-center mobile:px-3 mobile:py-2">
-          <h1 className="mobile:text-center tablet:text-start font-poppins font-bold text-primary mobile:text-[27px] tablet:text-[30px] tablet:leading-[1.2]">
+        <div className="mobile:w-full mobile:h-[60vh] tablet:h-full tablet:w-3/5 laptop:w-2/3 flex flex-col mobile:items-center tablet:items-start justify-center mobile:px-3 mobile:py-2">
+          <h1 className="mobile:text-center tablet:text-start font-poppins font-bold text-primary mobile:text-[27px] tablet:text-[30px] laptop:text-[34px] tablet:leading-[1.2]">
             Web & Mobile App Development
           </h1>
           <br />
-          <p className="mobile:text-center tablet:text-start font-medium font-inter text-txtColor mobile:text-[16px] mobile:leading-[1.2] tablet:text-[20px] tablet:leading-[1.2]">
+          <p className="mobile:text-center tablet:text-start font-medium font-inter text-txtColor mobile:text-[16px] mobile:leading-[1.2] tablet:text-[20px] laptop:text-[23px] tablet:leading-[1.2]">
             Your web and mobile Apps are pieces of the puszzle to grow your
             business. We use frameworks which tailor content and engagement
             methods to respond to different intents shown by your potential
@@ -30,9 +30,9 @@ function Services() {
         </div>
       </div>
       {/* Service 2 fraction:- Digital strategy part */}
-      <div className="w-full mobile:h-[90vh] tablet:h-[40vh] tablet:flex tablet:px-7">
+      <div className="w-full mobile:h-[90vh] tablet:h-[40vh] laptop:h-[75vh] tablet:flex tablet:px-7 laptop:px-0">
         {/* for magnifier image */}
-        <div className="mobile:w-full mobile:h-[35vh] tablet:h-full tablet:w-3/5 mobile:px-2 mobile:py-2 flex tablet:flex-col mobile:flex-row mobile:items-center tablet:items-start justify-center">
+        <div className="mobile:w-full mobile:h-[35vh] tablet:h-full tablet:w-3/5 mobile:px-2 laptop:pl-0 mobile:py-2 flex tablet:flex-col mobile:flex-row mobile:items-center tablet:items-start justify-center">
           {/* for mobiles */}
           <img
             src={Magnifier}
@@ -40,11 +40,11 @@ function Services() {
             className="mobile:w-4/5 tablet:hidden"
           />
           {/* for tablet onwards */}
-          <h1 className="mobile:hidden tablet:block tablet:text-start font-poppins font-semibold text-primary tablet:text-[30px] tablet:leading-[1.2]">
+          <h1 className="mobile:hidden tablet:block tablet:text-start font-poppins font-semibold text-primary tablet:text-[30px] laptop:text-[34px] tablet:leading-[1.2]">
             Digital Strategy Consulting
           </h1>
           <br />
-          <p className="mobile:hidden tablet:block tablet:text-start font-medium font-inter text-txtColor tablet:text-[20px] tablet:leading-[1.2]">
+          <p className="mobile:hidden tablet:block tablet:text-start font-medium font-inter text-txtColor tablet:text-[20px] laptop:text-[23px] tablet:leading-[1.2]">
             Your digital strategy should complement the overall marketing
             strategy of the company. In online marketing, each component will
             never work in isolation and every business needs a different mix. We
@@ -71,7 +71,7 @@ function Services() {
             efficient model for your business.
           </p>
           <br />
-          <button className="tablet:hidden bg-secondary text-center font-inter font-semibold mobile:text-[14px] text-accent1 mobile:py-3 mobile:px-6 rounded shadow-md active:bg-txtColorLight">
+          <button className="tablet:hidden bg-secondary text-center font-inter font-semibold mobile:text-[14px] text-accent1 mobile:py-3 mobile:px-6 laptop:pr-0 rounded shadow-md active:bg-txtColorLight">
             LEARN MORE
           </button>
           {/* for tablets onward */}
